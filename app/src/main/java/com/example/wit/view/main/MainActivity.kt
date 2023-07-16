2ccab20516d4965eff90458c9ea91f5f4da8b4e7
naheejae <gmlwo9845@gmail.com>
2023-07-16 18:48:01 +0900

    feat(mapview):BindMapActivity 추상화 제거, mapView 셋팅 완료

--- app/src/main/java/com/example/wit/view/main/MainActivity.kt
+++ app/src/main/java/com/example/wit/view/main/MainActivity.kt
@@ -3,28 +3,29 @@ package com.example.wit.view.main
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.example.wit.R
+import com.example.wit.base.view.activity.BindViewModelActivity
 import com.example.wit.databinding.ActivityMainBinding
 import com.naver.maps.geometry.LatLng
-import com.naver.maps.geometry.LatLngBounds
 import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapView
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.NaverMap.LAYER_GROUP_BUILDING
+import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.Marker
-import com.naver.maps.map.overlay.OverlayImage
 import com.naver.maps.map.util.MarkerIcons
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
-class MainActivity : BindMapActivity<ActivityMainBinding, MainViewModel>() {
+class MainActivity : BindViewModelActivity<ActivityMainBinding, MainViewModel>(), OnMapReadyCallback {
 
     override val layoutRes: Int
         get() = R.layout.activity_main
@@ -32,56 +33,72 @@ class MainActivity : BindMapActivity<ActivityMainBinding, MainViewModel>() {
         get() = ::showLoadingView
     override val hideLoadingView: (() -> Unit)?
         get() = ::hideLoadingView
-
     override val viewModel: MainViewModel by viewModels()
 
-    override var mapView: MapView? = null
-
-    override var naverMap: NaverMap? = null
+    private lateinit var mapView: MapView
+    private var naverMap: NaverMap? = null
+    private val coroutineExceptionHandler by lazy {
+        CoroutineExceptionHandler { _, exception ->
+            Log.e("BindMapActivity", "mapExceptionHandler::${exception.message}")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        mapView = binding.mvMain
+        binding.run {
+            mapView = mvMain
+            mapView.onCreate(savedInstanceState)
+            mapView.getMapAsync(this@MainActivity)
+        }
     }
 
-    override fun onMapReady(map: NaverMap) {
-        this.naverMap = map
-        Log.e("MainActivity", "onMapReady::$map")
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
 
-        lifecycleScope.launch(coroutineContext + coroutineExceptionHandler) {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
 
-                // mapview setting
-                naverMap?.run {
-                    mapType = NaverMap.MapType.Basic
-                    locationTrackingMode = LocationTrackingMode.Follow
-                    setLayerGroupEnabled(LAYER_GROUP_BUILDING, true)
-                    isIndoorEnabled = true
-                    uiSettings.run {
-                        isLocationButtonEnabled = true
-                    }
-                    addOnLocationChangeListener { location ->
-                        Log.e("NAVER_MAP", "addOnLocationChangeListener::${location.latitude}, ${location.longitude}")
-                    }
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
 
-                    val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.5670135, 126.9783740))
-                    this.moveCamera(cameraUpdate)
-                }
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+
+    override fun initView() {
+        binding.run {
+            btnMarker.setOnClickListener {
+                Log.e("MainActivity", "initView:: $it")
+                initMarker()
             }
         }
     }
 
-    override fun initView() {
-        binding.run {
-
-        }
-    }
-
     override fun initViewModel() {
-        lifecycleScope.launch(coroutineContext + exceptionHandler) {
+        lifecycleScope.launch(Dispatchers.Main + coroutineExceptionHandler) {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.run {
                     getToiletLocations()
@@ -92,40 +109,40 @@ class MainActivity : BindMapActivity<ActivityMainBinding, MainViewModel>() {
                         }
                     }.launchIn(lifecycleScope)
                 }
+            }
+        }
+    }
 
-                /* viewModel.sideEffects
-                     .onEach {
-                         Toast.makeText(this@MainActivity, it, Toast.LENGTH_SHORT).show()
-                     }
-                     .launchIn(lifecycleScope)
+    override fun onMapReady(map: NaverMap) {
+        Log.e("MainActivity", "onMapReady::$map")
 
-                 viewModel.state
-                     .onEach {
-                         when (it.loading) {
-                             true -> {
-                                 Log.e("Loading", it.toString())
-                             }
+        lifecycleScope.launch(Dispatchers.Main + coroutineExceptionHandler) {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                naverMap = map
+                naverMap?.run {
+                    mapType = NaverMap.MapType.Basic
+                    locationTrackingMode = LocationTrackingMode.Follow
+                    setLayerGroupEnabled(LAYER_GROUP_BUILDING, true)
+                    isIndoorEnabled = true
+                    uiSettings.isLocationButtonEnabled = true
+                    addOnLocationChangeListener { location ->
+                        Log.e("MainActivity", "LocationChange::${location.latitude}, ${location.longitude}")
+                    }
 
-                             false -> {
-                                 Log.e("Loading", it.toString())
-                             }
-                         }
-                     }
-                     .launchIn(lifecycleScope)*/
+                    val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.5670135, 126.9783740))
+                    moveCamera(cameraUpdate)
+                }
             }
         }
     }
 
     private fun initMarker() {
-        naverMap?.run {
-            val marker = Marker().apply {
-                icon = MarkerIcons.BLACK
-                iconTintColor = Color.RED
-                map = naverMap
-            }
-            marker.position = LatLng(37.5670135, 126.9783740)
-
-            Log.e("NAVER_MAP", "initMarker::${marker.position}")
+        Marker().run {
+            icon = MarkerIcons.BLACK
+            iconTintColor = Color.RED
+            position = LatLng(37.5670135, 126.9783740)
+            map = naverMap
+            Log.e("MainActivity", "initMarker::$position")
         }
     }
 }
